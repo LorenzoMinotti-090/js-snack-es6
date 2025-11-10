2cfd47f4f9c23db590bf0264f52a18f836274200
LorenzoMinotti-090 <minottilorenzo@gmail.com>
2025-11-10 15:51:48 +0100

    feat: bici

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("ciao mondo")
+
+
+const bici = [
+  { nome: "Toyota Grunge", peso: 12.5 },
+  { nome: "Nissan Tail", peso: 14.2 },
+  { nome: "Bmw P8", peso: 9.8 },
+  { nome: "Tesla Wind", peso: 11.3 },
+  { nome: "Porsche Urban", peso: 13.7 }
+];
+
+let biciLeggera = bici[0];
+
+for (let i = 1; i < bici.length; i++) {
+
+  if (bici[i]["peso"] < biciLeggera["peso"]) {
+    biciLeggera = bici[i];
+  }
+
+}
+
+console.log("La bici più leggera è:", biciLeggera["nome"], "con peso", biciLeggera["peso"], "kg");
